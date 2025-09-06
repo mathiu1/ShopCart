5fd674bb57412c1731baee44f7e58f02c13b4a07
mathivan07 <mathivan07@gmail.com>
2025-09-06 09:42:19 +0530

    ping file

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,32 @@ const app = require("./app");
 
 const connectDatabase = require("./config/database");
 
-
-
-
-
-
 connectDatabase();
 
-const server=app.listen(process.env.PORT || 5000,()=> {
-    console.log(`Server listening to the port :${process.env.PORT} in ${process.env.NODE_ENV} ` );
-})
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Server listening to the port :${process.env.PORT} in ${process.env.NODE_ENV} `
+  );
+});
 
-process.on("unhandledRejection",(err)=>{
-    console.log(`Error:${err.message}`);
-    console.log("Shutting down the server due to unhandled rejection");
-    server.close(()=>{
-        process.exit(1);
-    })
-})
 
-process.on("uncaughtException",(err)=>{
-    console.log(`Error:${err.message}`);
-    console.log("Shutting down the server due to uncaught Exception");
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+if (process.env.NODE_ENV === "Production") {
+  require("./ping.js");
+}
+
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log("Shutting down the server due to unhandled rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log("Shutting down the server due to uncaught Exception");
+  server.close(() => {
+    process.exit(1);
+  });
+});
